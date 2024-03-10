3f1fdd4d44c6c53a080d5a06c26081c420b13b4c
OleksiyM <oleksiymaliovanyy@gmail.com>
2024-03-10 23:37:39 +0200

    updates

--- entity/models.py
+++ entity/models.py
@@ -43,7 +43,7 @@ class User(Base):
     username: Mapped[str] = mapped_column((String(50)))
     email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    avatar: Mapped[str] = mapped_column(String(255), nullable=False)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
     refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
     updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now())
